05e663c7a2126d954536007777157879a2f24883
adam-conway <adam.n.conway@gmail.com>
2018-02-15 15:48:54 -0700

    Working on controller class & tests

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,28 +4,68 @@ require './lib/paths/hello'
 require './lib/paths/word_search'
 require './lib/paths/shutdown'
 require './lib/paths/datetime'
+require './lib/server'
+require './lib/response'
 
 class Controller
-  def initialize(verb, path)
-    @verb = verb
-    @path = path
+  def initialize(server)
+    @server = server
+    @response = Response.new(@server.request)
+    @hello = Hello.new
+    @game = Game.new
+    @verb = @response.verb
+    @path = @response.path
   end
 
-  def get_path(path)
-    case path
-    when '/' then [@verb, path]
-    when '/hello' then [@verb, path]
-    when '/datetime' then [@verb, path]
-    when '/shutdown' then [@verb, path]
-    when '/word_search' then [@verb, path]
-    when '/game' then [@verb, path]
+  def begin
+    inspect_incoming_request
+    response_to_client
+    finish_loop
+  end
+
+  def inspect_incoming_request
+    puts "Got this request:"
+    puts @server.request.inspect
+  end
+
+  def response_to_client
+    puts "Sending Response."
+    @pathing = route
+    header = @response.headers(@pathing.length)
+
+    @server.listener.puts header
+    @server.listener.puts @pathing
+  end
+
+  def finish_loop
+    puts ["Wrote this response:", @pathing].join("\n")
+    puts "\nResponse complete : Exiting."
+  end
+
+  def route
+    if @verb == 'GET'
+      get(@path)
+    else
+      post(@path)
     end
   end
 
-  def post_path(path)
+  def get(path)
     case path
-    when '/start_game' then
-    when '/game' then
+    when '/' then  @response.diagnostics
+    when '/hello' then @hello.call
+    when '/datetime' then Datetime.new.current_time
+    when '/shutdown' then Shutdown.new.close_server(@server.tcp_server, @server.total_requests)
+    when '/word_search' then WordSearch.new.check_dictionary(@response.parameter)
+    # when '/game' then '/game' #[@verb, path]
+    end
   end
+
+  # def post(path)
+  #   case path
+  #   when '/start_game' then
+  #   when '/game' then
+  #   end
+  # end
 
 end
--- lib/server.rb
+++ lib/server.rb
@@ -1,82 +1,95 @@
 require 'pry'
 require 'socket'
-require './lib/paths/game'
-require './lib/paths/hello'
-require './lib/paths/word_search'
-require './lib/paths/shutdown'
-require './lib/paths/datetime'
-require './lib/response'
+# require './lib/paths/game'
+# require './lib/paths/hello'
+# require './lib/paths/word_search'
+# require './lib/paths/shutdown'
+# require './lib/paths/datetime'
+# require './lib/response'
+require './lib/controller'
 
 class Server
-  attr_reader :tcp_server, :count
+  attr_reader :tcp_server, :request, :listener, :total_requests
   def initialize(port = 9292)
     @tcp_server = TCPServer.new(port)
     @tcp_server.listen(1)
-    @hello = Hello.new
-    @word_search = WordSearch.new
-    @shutdown = Shutdown.new
-    @datetime = Datetime.new
-    @total_count = 0
+    @total_requests = 0
+    # @controller = Controller.new
+    # @hello = Hello.new
+    # @word_search = WordSearch.new
+    # @shutdown = Shutdown.new
+    # @datetime = Datetime.new
+    # @total_count = 0
   end
 
   def start
     loop do
+      @total_requests += 1
       puts "Ready for a request"
       @request = []
       @listener = @tcp_server.accept
-      @total_count += 1
+      # @total_count += 1
       while line = @listener.gets and !line.chomp.empty?
         @request << line.chomp
       end
 
-      puts "Got this request:"
-      puts @request.inspect
+      Controller.new(self).begin
 
-      puts "Sending Response."
-      @response = Response.new(@request)
-      @post_data = @listener.read(@response.content_length.to_i)
-      @guess = @post_data.split[-2].to_i
 
-      path_response = path(@response.path, @response.verb)
 
-      header = @response.headers(path_response.length)
-
-      @listener.puts header
-      @listener.puts path_response
-      puts ["Wrote this response:", path_response].join("\n")
-      puts "\nResponse complete : Exiting."
+      # puts "Got this request:"
+      # puts @request.inspect
+      #
+      # puts "Sending Response."
+      # # @response = Response.new(@request)
+      # # build_new_controller unless @controller.is_a?(Controller)
+      # # # @controller = Controller.new(@response)
+      # # @post_data = @listener.read(@response.content_length.to_i)
+      # # @guess = @post_data.split[-2].to_i
+      # control_response = @controller.route
+      # #
+      # header = @response.headers(control_response.length)
+      #
+      # @listener.puts header
+      # @listener.puts control_response
+      # puts ["Wrote this response:", control_response].join("\n")
+      # puts "\nResponse complete : Exiting."
     end
     @listener.close
   end
 
-  def path(path, verb)
-    if verb == 'GET'
-      if path == '/'
-        @response.diagnostics
-      elsif path == '/hello'
-        @hello.call
-      elsif path == '/datetime'
-        @datetime.current_time
-      elsif path == '/shutdown'
-        @shutdown.close_server(@tcp_server, @total_count)
-      elsif path == '/word_search'
-        @word_search.check_dictionary(@response.parameter)
-      elsif path == '/game'
-        @game.number_of_guesses
-        @game.evaluate_guess
-      else
-        '404'
-      end
-    else
-      if path == '/start_game'
-        @game = Game.new
-        @game.start
-      elsif path == '/game'
-        @game.make_a_guess(@guess)
-        path('/game', 'GET')
-      else
-        '404'
-      end
-    end
-  end
+  # def build_new_controller
+  #   @controller = Controller.new(@response)
+  # end
+
+  # def path(path, verb)
+  #   if verb == 'GET'
+  #     if path == '/'
+  #       @response.diagnostics
+  #     elsif path == '/hello'
+  #       @hello.call
+  #     elsif path == '/datetime'
+  #       @datetime.current_time
+  #     elsif path == '/shutdown'
+  #       @shutdown.close_server(@tcp_server, @total_count)
+  #     elsif path == '/word_search'
+  #       @word_search.check_dictionary(@response.parameter)
+  #     elsif path == '/game'
+  #       @game.number_of_guesses
+  #       @game.evaluate_guess
+  #     else
+  #       '404'
+  #     end
+  #   else
+  #     if path == '/start_game'
+  #       @game = Game.new
+  #       @game.start
+  #     elsif path == '/game'
+  #       @game.make_a_guess(@guess)
+  #       path('/game', 'GET')
+  #     else
+  #       '404'
+  #     end
+  #   end
+  # end
 end
