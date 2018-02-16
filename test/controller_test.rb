3282f8dd263f751ad02f597726ad000f1c31d3f5
adam-conway <adam.n.conway@gmail.com>
2018-02-16 00:16:03 -0700

    Improving controller testing

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -3,10 +3,58 @@ require './lib/controller'
 require './lib/server'
 
 class ControllerTest < MiniTest::Test
+  def test_it_exists
+    skip
+    controller = Controller.new("Hello")
+
+    assert_instance_of Controller, controller
+  end
+
   def test_root
-    response = Faraday.get 'http://127.0.0.1:9292/'
+    skip
+    response = Faraday.get "http://127.0.0.1:9292/"
+    substring = 'Host: Faraday'
 
-    assert_equal '', response
+    assert response.body.include?(substring)
+  end
+
+  def test_hello
+    skip
+    response = Faraday.get 'http://127.0.0.1:9292/hello'
+
+    assert_equal '<html>Hello World!(1)</html>', response.body
+
+    response = Faraday.get 'http://127.0.0.1:9292/hello'
+
+    assert_equal '<html>Hello World!(2)</html>', response.body
+  end
+
+  def test_datetime
+    skip
+    response = Faraday.get 'http://127.0.0.1:9292/datetime'
+    expected = Time.now.strftime('%I:%M%p on %A, %B %d, %Y')
+
+    assert response.body.include?(expected)
+  end
+
+  def test_shutdown
+    skip
+    response = Faraday.get 'http://127.0.0.1:9292/shutdown'
+    expected = "Total Requests: #{requests}"
+
+    assert_equal expected, response
+  end
+
+  def test_word_search
+    skip
+    response = Faraday.get 'http://127.0.0.1:9292/word_search?word=help'
+    expected = "<html>help is a known word</html>"
+
+    assert_equal expected, response.body
+  end
+
+  def test_game
+    response = Faraday.get 'http://127.0.0.1:9292/game'
   end
 
   def test_
@@ -14,28 +62,7 @@ class ControllerTest < MiniTest::Test
     response = Faraday.get 'http://127.0.0.1:9292/'
   end
 
-  def test_
-    skip
-    response = Faraday.get 'http://127.0.0.1:9292/'
-  end
-
-  def test_
-    skip
-    response = Faraday.get 'http://127.0.0.1:9292/'
-  end
-
-  def test_
-    skip
-    response = Faraday.get 'http://127.0.0.1:9292/'
-  end
-
-  def test_
-    skip
-    response = Faraday.get 'http://127.0.0.1:9292/'
-  end
-
-  def test_
-    skip
-    response = Faraday.get 'http://127.0.0.1:9292/'
-  end
+  # def teardown
+  #   Shutdown.new.close_server(@server.tcp_server, @server.total_requests)
+  # end
 end
